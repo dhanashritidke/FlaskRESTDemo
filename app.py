980730e2005bc495ef92493a23f5c977370758dc
dhanashri <dhana.tidke@gmail.com>
2018-03-01 20:31:44 -0800

    Modified

---
+++ app.py
@@ -0,0 +1,31 @@
+import csv
+import json
+from flask import Flask
+from flask import request
+
+data={}
+with open('ra_data_classifier.csv', 'r', encoding='mac_roman') as csvfile:
+    fileReader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    header=None
+    for index,row in enumerate(fileReader):
+        if index == 0:
+            header=row
+            continue
+        r={}
+        for _,head in enumerate(row):
+            r[header[_]]=head
+        data[row[0]]=r
+
+app = Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def getJSON():
+    content = request.get_json(silent=True)
+    return json.dumps(data[content["hid"]])
+
+@app.route("/hid/<hid>",methods=['GET','POST'])
+def getJSONwithHid(hid):
+    if hid in data.keys():
+        return json.dumps(data[hid])
+    else:
+        return {}
